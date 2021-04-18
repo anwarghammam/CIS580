8de34d0594cc2fb3720ca661fd8f0f9036f6e668
anwarghammam <anwarghammam582@gmail.com>
2021-04-19 00:11:50 +0100

    add constraints

---
+++ source-code/jmetal/Constrained Approach/API.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  8 09:50:11 2021
+
+@author: anwar
+"""
+
+from flask import request
+from flask import Flask, jsonify
+import json
+import subprocess
+from run1 import transform
+from flask_restful import  Api
+from flask_cors import CORS
+app = Flask(__name__)
+api = Api(app)
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
+
+
+@app.route('/getjson/', methods=['GET'])
+def summary():
+    f = open(r"./data.json")
+
+    data = json.load(f)
+    response = app.response_class(
+        response=json.dumps(data),
+        mimetype='application/json'
+    )
+    return response
+
+
+
+@app.route('/update/', methods=['POST'])
+def events():
+    
+
+    content = request.get_data()
+    c=json.loads(content)
+    print(c)
+    with open('./data.json', 'w') as f:
+        json.dump(c, f)
+    return 'JSON posted'
+
+
+
+@app.route('/default/', methods=['GET'])
+def get():
+    cmd = ('ssh root@manager docker stack deploy --compose-file docker-compose_initial.yml p1').split()
+
+    p = subprocess.Popen(cmd,stdout = subprocess.PIPE)
+    output, errors = p.communicate()
+       
+    print(output)
+    print(errors)
+    result=jsonify("done")
+       
+    return (result)
+
+@app.route('/newapproach/', methods=['GET'])
+
+def new_approach():
+    transform()
+    # cmd = ('docker-machine ssh manager docker stack deploy --compose-file docker-compose1.yml D ').split()
+
+    # p = subprocess.Popen(cmd)
+    # output, errors = p.communicate() 
+       
+    # print(output)
+    # print(errors)
+#        ssh.connect(hostname='ec2-54-87-55-164.compute-1.amazonaws.com', username='ubuntu',pkey=privkey)
+#        #ssh.connect("ubuntu@ec2-54-87-55-164.compute-1.amazonaws.com",)
+#        stdin, stdout, stderr=ssh.exec_command('rmdir test')
+#        print (stdout.read())
+#        print(stderr.read())
+       
+
+       
+    result=jsonify("done")
+       
+    return (result)  
+
+
+
+if __name__ == '__main__':
+     app.run(port='5002')       
